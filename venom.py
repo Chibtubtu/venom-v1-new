52289bc58171a7a32bed4c60cafe15803d550ec9
Chibtubtu <chibtuchintu544@gmail.com>
2024-09-08 00:39:51 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7510589975:AAGcnpp_KedlDqA2rgyp1bNdJNmCQFO23mc'
+ADMIN_ID = '6508557076'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
